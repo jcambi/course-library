0e460a2ff000984d255a422dd2aa225a851a1b11
John Cambi <john.cambi@fleetcomplete.com>
2019-12-25 21:46:50 -0500

    add handle exception message

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -39,6 +39,16 @@ namespace CourseLibrary.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            } else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later");
+                    });
+                });
             }
 
             app.UseRouting();
